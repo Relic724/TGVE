3971de6ed7c61e07d7055e4ba31054092f86807d
Relic724 <relic724@gmail.com>
2014-06-21 05:35:39 -0500

    added a module to the module suite

---
+++ TGVEModuleSuite/TGVEMultiview/src/com/iteag/multiview/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@TemplateRegistration(folder = "Other", content = "TGVEmvTemplate.tgve")
+package com.iteag.multiview;
+
+import org.netbeans.api.templates.TemplateRegistration;
