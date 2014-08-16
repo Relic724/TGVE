750997bd65a798ef11b208ceee012cb544b1afc6
Relic724 <relic724@gmail.com>
2014-08-16 17:21:55 -0500

    First Tonegod GUI rendering success
    Works on third try tab switching... timing is still bad

--- TGVEModuleSuite/TgveFileType/src/com/iteag/tgvefiletype/TgveVisualElement2.java
+++ TGVEModuleSuite/TgveFileType/src/com/iteag/tgvefiletype/TgveVisualElement2.java
@@ -4,17 +4,20 @@
  */
 package com.iteag.tgvefiletype;
 
-import com.jme3.app.Application;
-import com.jme3.app.SimpleApplication;
+
+import com.jme3.font.BitmapFont;
+import com.jme3.gde.core.assets.ProjectAssetManager;
+import com.jme3.gde.core.scene.PreviewRequest;
+import com.jme3.gde.core.scene.SceneApplication;
+import com.jme3.gde.core.scene.SceneListener;
+import com.jme3.gde.core.scene.SceneRequest;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector2f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
-import com.jme3.system.AppSettings;
-import com.jme3.system.JmeCanvasContext;
-import com.jme3.system.JmeSystem;
-import java.awt.Canvas;
-import java.util.logging.Level;
+import java.awt.Color;
 import java.util.logging.Logger;
 import javax.swing.Action;
 import javax.swing.JComponent;
@@ -27,6 +30,13 @@ import org.openide.awt.UndoRedo;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle.Messages;
 import org.openide.windows.TopComponent;
+import java.awt.Component;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+import tonegod.gui.core.Screen;
+import tonegod.gui.controls.windows.Window;
+
 /**
  *
  * @author charles
@@ -39,56 +49,49 @@ import org.openide.windows.TopComponent;
         preferredID = "TgveVisual",
         position = 3000)
 @Messages("LBL_Tgve_VISUAL2=Absolute")
-public final class TgveVisualElement2 extends JPanel implements MultiViewElement{
+public class TgveVisualElement2 extends JPanel implements MultiViewElement, 
+  SceneListener{
     
     private TgveDataObject obj;
     private JToolBar toolbar = new JToolBar();
+    private SceneRequest request;
+    private ProjectAssetManager pam;
+    private Node absoluteNode = new Node("absoluteNode");
     private transient MultiViewElementCallback callback;
-    private SimpleApplication app;
-    private JmeCanvasContext context;
-    private Canvas canvas;
+    private Component oglCanvas;
     private static final Logger logger = Logger.getLogger(TgveVisualElement2.class.getName());
+    private Screen screen;
 
+    private static ComponentAdapter tgveComponentListener = new ComponentAdapter(){
+        @Override
+         public void componentResized(final ComponentEvent e) {
+            logger.info(e.getComponent().getClass().getName() + " resized to " + 
+                    e.getComponent().getWidth() + ", " + e.getComponent().getHeight());
+        }
+        
+        @Override
+        public void componentMoved(final ComponentEvent e) {
+            logger.info(e.getComponent().getClass().getName() + " moved, but resized to " + 
+                    e.getComponent().getWidth() + ", " + e.getComponent().getHeight());
+        }
+        
+        
+    };
+    
     public TgveVisualElement2(Lookup lkp) {
         obj = lkp.lookup(TgveDataObject.class);
         assert obj != null;
-        
-        AppSettings settings = new AppSettings(true);
-        
-        settings.setWidth(1024);
-        settings.setHeight(960);
-        settings.setAudioRenderer(null);
-        JmeSystem.setLowPermissions(true);
-        
-                
-        defineApp();
-        app.setSettings(settings);
-        app.createCanvas();
-        
-        context = (JmeCanvasContext) app.getContext();
-        canvas = context.getCanvas();
-        canvas.setSize(100, 100);
-        
-        
-        logger.log(Level.INFO, "App has been started");
+        setMinimumSize(new java.awt.Dimension(24, 24));
+        setPreferredSize(new java.awt.Dimension(400, 300));
+        setLayout(new java.awt.GridLayout(1,0));
+        setBackground(Color.DARK_GRAY);
+        setFocusable(false);
+        this.addComponentListener(tgveComponentListener);
+        logger.info("TGVE Initialization complete");
         
     }
-
-    public void defineApp(){
-        app = new SimpleApplication() {
-            
-            @Override
-            public void simpleInitApp() {
-                Box boxMesh = new Box(1,1,1);
-                Geometry boxy = new Geometry("boxy", boxMesh);
-                Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-                material.setColor("Color", ColorRGBA.Blue);
-                boxy.setMaterial(material);
-                rootNode.attachChild(boxy);
-                
-            }
-        };
-    }
+    
+    
     
     @Override
     public String getName() {
@@ -117,36 +120,59 @@ public final class TgveVisualElement2 extends JPanel implements MultiViewElement
 
     @Override
     public void componentOpened() {
-       // TODO: Initialize and fire up the JME3
-       add(canvas); 
+       // TODO: Initialize and fire up the JME3... properly
+        pam = new ProjectAssetManager();
+        SceneApplication.getApplication().addSceneListener(this);
+        
+       // TODO: Load Tonegod Objects from TgveDataObject
+        Box boxMesh = new Box(1,1,1);
+        Geometry box = new Geometry("blue box", boxMesh);
+        Material material = new Material(SceneApplication.getApplication().getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+        material.setColor("Color", ColorRGBA.Blue);
+        box.setMaterial(material);
+        absoluteNode.attachChild(box);
+        request = new SceneRequest(this, absoluteNode, pam);
+
+        logger.info("TGVE Component Opened");
+        
     }
 
     @Override
     public void componentClosed() {
         //TODO: Stop the app
-       remove(canvas);
-       app.stop();
+       SceneApplication.getApplication().removeSceneListener(this);
     }
 
     @Override
     public void componentShowing() {
         // TODO: App is running, set the surface; if paused, unpause
-        canvas.setSize(getWidth(),getHeight());
-        app.startCanvas();
-        
+        try {
+            oglCanvas = SceneApplication.getApplication().getMainPanel();
+            oglCanvas.setFocusable(false);
+            add(oglCanvas);
+        } catch (Exception e) { 
+           SceneApplication.showStartupErrorMessage(e); 
+        } catch (Error err) { 
+             SceneApplication.showStartupErrorMessage(err); 
+        } 
+    
+        SceneApplication.getApplication().openScene(request);
+        logger.info("TGVE Component Showing");
     }
 
     @Override
     public void componentHidden() {
         // TODO: App is running, pause the engine
-        app.stop();
+        remove(oglCanvas);
+        SceneApplication.getApplication().closeScene(request);
+        logger.info("TGVE Component Hidden");
 
     }
 
     @Override
     public void componentActivated() {
         // TODO: accept input handlings here
-        app.getFlyByCamera().setDragToRotate(true);
+        
     }
 
     @Override
@@ -168,5 +194,36 @@ public final class TgveVisualElement2 extends JPanel implements MultiViewElement
     public CloseOperationState canCloseElement() {
         return CloseOperationState.STATE_OK;
     }
+
+    @Override
+    public void sceneOpened(SceneRequest theSceneRequest) {
+        if(theSceneRequest.getRequester() == this){
+            screen = new Screen(SceneApplication.getApplication());
+            SceneApplication.getApplication().getGuiNode().addControl(screen);
+            Window win = new Window(screen, "Sample Window", new Vector2f(10,10));
+            win.setText("t0neg0d GUI Window");
+            win.setTextVAlign(BitmapFont.VAlign.Center);
+            win.setTextAlign(BitmapFont.Align.Center);
+            screen.addElement(win);
+            
+            logger.info("TGVE recieved a request");
+            
+        }
+    }
+
+    @Override
+    public void sceneClosed(SceneRequest theSceneRequest) {
+        if(theSceneRequest.getRequester() == this){
+            SceneApplication.getApplication().getGuiNode().removeControl(screen);
+            screen = null;
+            logger.info("TGVE recieved a request to Close");
+        }
+    }
+
+    @Override
+    public void previewCreated(PreviewRequest pr) {
+    }
+
+    
     
 }
